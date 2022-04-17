68a1fc5a81dd837d28c67d083f697a0487d9b84e
Nathanael Carauna <nathanael.carauna@gmail.com>
2022-04-17 19:55:59 -0300

    Feature -> Shopping cart page listing carts by user, price changes based on quantity

--- BulkyBook.DataAccess/Repository/Repository.cs
+++ BulkyBook.DataAccess/Repository/Repository.cs
@@ -27,15 +27,20 @@ namespace BulkyBook.DataAccess.Repository
         }
 
         //includeProp - "Category,CoverType"
-        public IEnumerable<T> GetAll(string? includeProperties = null)
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
-            if(includeProperties != null)
+            if (filter != null)
             {
-                foreach(var includeProp in includeProperties.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries))
+                query = query.Where(filter);
+
+            }
+            if (includeProperties != null)
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
                 {
-                    query = query.Include(includeProp); 
-                }                
+                    query = query.Include(includeProp);
+                }
             }
             return query.ToList();
         }
